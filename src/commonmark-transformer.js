53c067a0d4d2e2e38c170b063dfa968a37ec1128
Kyle Kelley <rgbkrk@gmail.com>
2015-07-23 10:26:39 -0700

    Use commonmark for the greater good.

--- src/commonmark-transformer.js
+++ src/commonmark-transformer.js
@@ -1,14 +1,27 @@
 "use strict";
 
+var commonmark = require("commonmark");
+
 export class MarkdownTransformer {
+    constructor() {
+        this.reader = new commonmark.Parser();
+        this.writer = new commonmark.HtmlRenderer({
+            safe: true
+        });
+    }
+
     get mimetype() {
         return "text/markdown";
     }
 
     transform(data, doc) {
         var div = doc.createElement("div");
+        var parsed = this.reader.parse(data);
 
-        // Hand off to commonmark.js
+        // TODO: Any other transformations on the parsed object
+        // See https://github.com/jgm/commonmark.js#usage
+
+        div.innerHTML = this.writer.render(parsed);
 
         return div;
     }
