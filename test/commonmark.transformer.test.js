4581598d437764696b30cbe31326d80e99730955
Kyle Kelley <rgbkrk@gmail.com>
2015-08-01 10:28:45 -0500

    There must be a space after a # in commonmark...

--- test/commonmark.transformer.test.js
+++ test/commonmark.transformer.test.js
@@ -13,8 +13,8 @@ describe('text transformer', function() {
         assert.equal(markdownTransform.mimetype, "text/markdown");
     });
     describe('#transform', function() {
-        it('should create a div with some happy markdown', function() {
-            let text = `#Ratification of Markdown Association
+        it('should create a div with nice markup', function() {
+            let text = `# Ratification of Markdown Association
 
 We are present here because we believe in unification
 across all markdowns, whether common, flavored like
@@ -36,7 +36,26 @@ We need markdown to work
 
             let el = markdownTransform('text/markdown', text, this.document);
 
-            assert.equal(el.innerHTML, '<p>#Ratification of Markdown Association</p>\n<p>We are present here because we believe in unification\nacross all markdowns, whether common, flavored like\nGitHub, or used to help an undergrad do their homework\nvia StackOverflow. It is of <em>great importance</em> that we\nunify for the <a href="https://jupyter.org">sake of the sciences</a>,\nthe arts, and be able to express in rich text our thoughts,\nfeelings, and desires.</p>\n<h2>Speaking for some</h2>\n<p>We need markdown to work</p>\n<ul>\n<li>On the web</li>\n<li>On your hard drive</li>\n<li>Hanging upside down from a bridge</li>\n<li>In letters to your lovers</li>\n</ul>\n<pre><code class="language-bash">npm install transformime-commonmark\n</code></pre>\n');
+            assert.equal(el.innerHTML, `<h1>Ratification of Markdown Association</h1>
+<p>We are present here because we believe in unification
+across all markdowns, whether common, flavored like
+GitHub, or used to help an undergrad do their homework
+via StackOverflow. It is of <em>great importance</em> that we
+unify for the <a href="https://jupyter.org">sake of the sciences</a>,
+the arts, and be able to express in rich text our thoughts,
+feelings, and desires.</p>
+<h2>Speaking for some</h2>
+<p>We need markdown to work</p>
+<ul>
+<li>On the web</li>
+<li>On your hard drive</li>
+<li>Hanging upside down from a bridge</li>
+<li>In letters to your lovers</li>
+</ul>
+<pre><code class="language-bash">npm install transformime-commonmark
+</code></pre>
+`);
+            assert.equal(el.localName, 'div');
 
         });
     });
